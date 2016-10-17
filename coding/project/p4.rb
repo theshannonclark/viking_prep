84cfc116f04c1613dc445e38c17647897355e5da
Shannon Clark <theshannonclark@gmail.com>
2016-10-17 14:38:06 +1100

    Finish project problem 4

---
+++ coding/project/p4.rb
@@ -0,0 +1,18 @@
+# problem 4: combinations
+
+# Write a method combinations which takes two arrays of strings
+# and returns an array with all of the combinations of the items
+# in them, listing the first items first.
+
+def combinations(array1, array2)
+	result = []
+	array1.each do |word1|
+		array2.each do |word2|
+			result.push("#{word1}#{word2}")
+		end
+	end
+	return result
+end
+
+p combinations(["on","in"],["to","rope"])
+# => [“onto”,”onrope”,”into”,”inrope”]
