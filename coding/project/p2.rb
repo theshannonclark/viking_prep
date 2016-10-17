952d2012195c11421e14ad1a6adcf786c9d13589
Shannon Clark <theshannonclark@gmail.com>
2016-10-17 14:25:14 +1100

    Finish project problem 2

---
+++ coding/project/p2.rb
@@ -0,0 +1,16 @@
+# problem 2: factorial
+
+# Write a method factorial which takes a number and returns the product
+# of every number up to the current number multiplied together.
+
+def factorial(integer)
+	product = 1
+	(2..integer).each do |i|
+		product *= i
+	end
+	return product
+end
+
+puts factorial(5)
+# => 120
+# (from 1*2*3*4*5)
