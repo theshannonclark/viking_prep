e800cfe06fcf9f15961eb28fb67460719771fbee
Shannon Clark <theshannonclark@gmail.com>
2016-10-24 14:48:11 +1100

    More indentation
    Oh, sublime, why are you doing this to me?

--- coding/project/p5.rb
+++ coding/project/p5.rb
@@ -5,25 +5,25 @@
 
 
 def is_prime?(number)
-	result = true
+  result = true
 
-	# 2 is the only even prime number
-	if number == 2
-		result = true
-	elsif number < 2 or number % 2 == 0
-		result = false
-	else
-		# if there aren't any multiples of n before the square root
-		# of n, there aren't any at all.
-		root = Math.sqrt(number).ceil
-		(3..root).each do |i|
-			if number % i == 0
-				result = false
-				break
-			end
-		end
-	end
-	return result
+  # 2 is the only even prime number
+  if number == 2
+    result = true
+  elsif number < 2 or number % 2 == 0
+    result = false
+  else
+    # if there aren't any multiples of n before the square root
+    # of n, there aren't any at all.
+    root = Math.sqrt(number).ceil
+    (3..root).each do |i|
+      if number % i == 0
+        result = false
+        break
+      end
+    end
+  end
+  return result
 end
 
 puts is_prime?(1)
--- coding/project/p6.rb
+++ coding/project/p6.rb
@@ -9,24 +9,24 @@
 # otherwise overwrite each other.
 
 def left_corner_inside?(rectangle, point)
-	return ((point[0] >= rectangle[0][0]  and
-		     point[0] < rectangle[1][0]) and
-		    (point[1] >= rectangle[0][1]  and
-		     point[1] < rectangle[1][1]))
+  return ((point[0] >= rectangle[0][0]  and
+         point[0] < rectangle[1][0]) and
+        (point[1] >= rectangle[0][1]  and
+         point[1] < rectangle[1][1]))
 end
 
 def right_corner_inside?(rectangle, point)
-	return ((point[0] > rectangle[0][0]  and
-		     point[0] <= rectangle[1][0]) and
-		    (point[1] > rectangle[0][1]  and
-		     point[1] <= rectangle[1][1]))
+  return ((point[0] > rectangle[0][0]  and
+         point[0] <= rectangle[1][0]) and
+        (point[1] > rectangle[0][1]  and
+         point[1] <= rectangle[1][1]))
 end
 
 def overlap(rectangle1, rectangle2)
-	return ((left_corner_inside?(rectangle2, rectangle1[0]) or
-		     right_corner_inside?(rectangle2, rectangle1[1])) or
-		    (left_corner_inside?(rectangle1, rectangle2[0]) or
-		     right_corner_inside?(rectangle1, rectangle2[1])))
+  return ((left_corner_inside?(rectangle2, rectangle1[0]) or
+         right_corner_inside?(rectangle2, rectangle1[1])) or
+        (left_corner_inside?(rectangle1, rectangle2[0]) or
+         right_corner_inside?(rectangle1, rectangle2[1])))
 end
 
 puts overlap([[0, 0], [3, 3]], [[1, 1], [4, 5]])
